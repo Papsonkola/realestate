42705256ec3cf422bbfa4c0a308e34da2384abb5
papsonkola <papsonkola@gmail.com>
2023-03-15 16:28:22 +0100

    new estate

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -15,10 +15,10 @@ function Navbar() {
             <h1><span><BsFillHouseFill/>Papson</span>Estate</h1>
             <button className='btn'>Sign In</button>
             <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                <li><a href='#'>Home</a></li>
-                <li><a href='#'>Search</a></li>
-                <li><a href='#'>About</a></li>
-                <li><a href='#'>Contact</a></li>
+                <li><a href='#home'>Home</a></li>
+                <li><a href='#search'>Search</a></li>
+                <li><a href='#about'>About</a></li>
+                <li><a href='#contact'>Contact</a></li>
             </ul>
             <div className='hamburger' onClick={handleClick}>
                 {click ? (<FaRegTimesCircle className='icon'/>) : (<HiOutlineMenuAlt4 className='icon'/>)}
